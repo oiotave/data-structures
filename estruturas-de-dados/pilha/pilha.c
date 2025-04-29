780a314c417e3bd07d1d9119e3be09262a2d3246
Jotavê <oiotave@gmail.com>
2025-04-28 21:15:09 -0300

    Update pilha.c

--- estruturas-de-dados/pilha/pilha.c
+++ estruturas-de-dados/pilha/pilha.c
@@ -7,30 +7,24 @@ Stack* new_stack() {
 }
 
 int is_empty(Stack *s) {
-    if(!s)
-        return 1;
-    else
-        return 0;
+    if(!s) return 1;
+    else   return 0;
 }
 
 int is_unitary(Stack *s) {
-    if(s->next == NULL)
-        return 1;
-    else
-        return 0;
+    if(s->next == NULL) return 1;
+    else return 0;
 }
 
 void push(Stack **pstack, int num) {
     Stack *aux;
-
-    // Alocação de novo elemento
-    aux = (Stack*) malloc(sizeof(Stack));
+    
+    aux = (Stack*) malloc(sizeof(Stack)); // AlocaÃ§Ã£o de novo elemento
 
     if(!aux) {
         printf("\nErro de alocacao\n");
         return;
     }
-
     aux->info = num;
     aux->next = *pstack;
     *pstack = aux;
@@ -39,8 +33,7 @@ void push(Stack **pstack, int num) {
 int pop(Stack **pstack) {
     Stack *aux;
 
-    if(is_empty(*pstack))
-        printf("\nPilha vazia\n");
+    if(is_empty(*pstack)) printf("\nPilha vazia\n");
 
     else {
         int num = (*pstack)->info;
@@ -61,7 +54,6 @@ void clear(Stack **pstack) {
     while(aux) {
         aux2 = aux;
         aux = aux->next;
-
         free(aux2);
     }
 }
